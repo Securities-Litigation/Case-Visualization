3d5f17647ea6ab9119d4cd1f94a25ee80eccea96
Matt <08martinm@gmail.com>
2016-10-08 21:16:59 -0700

    feat: running webpack-dev-server and node servers on separate ports for dev purposes (enables fast switching between hot-reload and nodemon)

--- server/index.js
+++ server/index.js
@@ -1,17 +1,39 @@
-const app = require('./server');
-// const db = require('./db');
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
 
-// db.sync({force: process.env.DROPTABLE})
-// .then(findOrCreateCategories)
-// .then(startServer);
+const utils = require('./config/utils');
 
-function startServer() {
-  app.listen(port, function() {
-    console.log('Server is listening on port', port);
-  });
-}
+let app = express();
+let PORT = 3000;
 
-startServer();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client/public'));
+app.get('/simple', function(req, res) {
+  res.send('Hello world\n');
+});
 
-module.exports = app;
+app.listen(PORT, function() {
+  console.log('Running on port ', PORT);
+});
+
+// we start a webpack-dev-server with our config
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('../webpack.config.js');
+ 
+new WebpackDevServer(webpack(config), {
+   publicPath: config.output.publicPath,
+   hot: true,
+   historyApiFallback: true,
+   proxy: {
+     "*": "http://localhost:3000"
+   }
+}).listen(3001, 'localhost', function (err, result) {
+   if (err) {
+     console.log(err);
+   }
+
+   console.log('Listening at localhost:3001');
+});
