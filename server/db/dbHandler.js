e1f52e15437f8aefa4c818d0757b98ab9b2e8e0a
Matt <08martinm@gmail.com>
2016-10-13 09:25:54 -0700

    feat: replaced mySQL with mongo

--- server/db/dbHandler.js
+++ server/db/dbHandler.js
@@ -1,17 +1,46 @@
-var mysql = require('mysql');
+var MongoClient = require('mongodb').MongoClient;
+var initialCase = {
+  case: 'Akorn',
+  data: {
+    class: {
+      beg: '2012-01-01',
+      end: '2015-12-31'
+    },
+    control: {
+      beg: '2015-01-01',
+      end: '2015-12-31'
+    },
+    scenarios: {
+      1: {
+        1: '2016-01-02', 
+      },
+      2: {
+        1: '2016-10-02',
+        2: '2016-01-15'
+      },
+      3: {
+        1: '2016-10-02',
+        2: '2016-01-15',
+        3: '2016-01-18'
+      },
+      4: 'AddDrop'
+    },
+    companyInfo: {
+      ticker: 'AKRX',
+      exchange: 'NASDAQ',
+      percentReduction: 50
+    }
+  }
+};
 
-var connection = mysql.createConnection({
-  user: 'root',
-  password: process.env.DB_PW,
-  host: 'localhost',
-  database: 'SecLit'
+MongoClient.connect("mongodb://localhost:27017/SecLit", function(err, db) {
+  if (!err) { console.log("Connected to mongodb"); }
+  db.createCollection('inputData', function(err, collection) {});
+  var inputs = db.collection('inputData');
+  inputs.insert(initialCase, {w:1}, function(err, result) {});
 });
 
-// connection.connect(function(error) {
-//   if (error) {
-//     throw error;
-//   }
-//   console.log('database connection established');
-// });
 
-module.exports = connection;
+
+
+
